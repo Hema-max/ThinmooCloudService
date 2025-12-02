9462f3636ced24de35936b3e6de079a1dc36877b
Hema <hemanavi75@gmail.com>
2025-12-02 23:02:10 +0530

    non-blocking

--- server.js
+++ server.js
@@ -173,7 +173,9 @@ async function start() {
         await sequelize.authenticate();
         console.log('✅ Database connected successfully.');
 
-        await sequelize.sync({ alter: true });
+        // await sequelize.sync({ alter: true });
+        await sequelize.sync(); // fast, non-blocking
+
         console.log('✅ Tables synced');
 
         const PORT = process.env.PORT || 5000;
